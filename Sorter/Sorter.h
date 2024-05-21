0f31e8961936933f44457b16ebab2085b77e341e
TyHryCkA <palayf@mail.ru>
2024-05-21 22:15:35 +0300

    edits
    delete useless method

--- Sorter/Sorter.h
+++ Sorter/Sorter.h
@@ -14,26 +14,24 @@
 template<typename T>
 class Sorter {
 public:
-    Sorter(int M, const std::string& input_file_path);
+    Sorter(int M, const std::string &input_file_path);
 
-    Sorter(int M, const std::string& input_file_path, const std::string& config_path);
+    Sorter(int M, const std::string &input_file_path, const std::string &config_path);
 
     void sort(std::string outFilePath);
 
 private:
     int M{};
-    T* _output_tape;
-    T* _input_tape;
-    T* _first_temp_tape;
-    T* _second_temp_tape;
-    T* _first_trash_tape;
-    T* _second_trash_tape;
-
-    void merge_tapes(int size_of_target_tape, int size_of_blocks, T* target_tape);
+    T *_output_tape;
+    T *_input_tape;
+    T *_first_temp_tape;
+    T *_second_temp_tape;
+    T *_first_trash_tape;
+    T *_second_trash_tape;
 };
 
 template<typename T>
-Sorter<T>::Sorter(int M, const std::string& input_file_path) {
+Sorter<T>::Sorter(int M, const std::string &input_file_path) {
     this->M = M;
     _input_tape = new T(input_file_path);
     std::ofstream file;
@@ -52,7 +50,7 @@ Sorter<T>::Sorter(int M, const std::string& input_file_path) {
 }
 
 template<typename T>
-Sorter<T>::Sorter(int M, const std::string& input_file_path, const std::string& config_path) {
+Sorter<T>::Sorter(int M, const std::string &input_file_path, const std::string &config_path) {
     this->M = M;
     _input_tape = new T(input_file_path);
     std::ofstream file;
@@ -71,41 +69,32 @@ Sorter<T>::Sorter(int M, const std::string& input_file_path, const std::string& 
 }
 
 template<typename T>
-void Sorter<T>::merge_tapes(int size_of_target_tape, int size_of_blocks, T* target_tape) {
-    (*_first_temp_tape).reset();
-    (*_second_temp_tape).reset();
-    for(int i = 0; i < size_of_target_tape / size_of_blocks; i++){
-
-    }
-}
-
-template<typename T>
 void Sorter<T>::sort(std::string outFilePath) {
     _output_tape = new T(outFilePath);
     int block_size = M;
     int total_blocks = 0;
     std::vector<int> memory(block_size);
     int N = 0;
-    while(!(*_input_tape).is_empty()){
+    while (!(*_input_tape).is_empty()) {
         total_blocks++;
-        for(int i = 0; i < block_size; i++){
+        for (int i = 0; i < block_size; i++) {
             memory[i] = (*_input_tape).get_number();
             N++;
             (*_input_tape).go_forward();
-            if((*_input_tape).is_empty()){
+            if ((*_input_tape).is_empty()) {
                 std::sort(memory.begin(), memory.begin() + i + 1);
-                for(int j = 0; j < i + 1; j++){
+                for (int j = 0; j < i + 1; j++) {
                     (*_first_temp_tape).set_number(memory[j]);
                     (*_first_temp_tape).go_forward();
                 }
                 break;
             }
         }
-        if((*_input_tape).is_empty()){
+        if ((*_input_tape).is_empty()) {
             break;
         }
         std::sort(memory.begin(), memory.end());
-        for(int i = 0; i < block_size; i++){
+        for (int i = 0; i < block_size; i++) {
             (*_first_temp_tape).set_number(memory[i]);
             (*_first_temp_tape).go_forward();
         }
@@ -114,10 +103,10 @@ void Sorter<T>::sort(std::string outFilePath) {
     int first_tape_size = (N / block_size / 2 + N / block_size % 2) * block_size;
     int second_tape_size = (N / block_size / 2) * block_size + N % block_size;
     int new_number;
-    for(int i = 0; i < first_tape_size; i++){
+    for (int i = 0; i < first_tape_size; i++) {
         (*_first_temp_tape).go_forward();
     }
-    while(!(*_first_temp_tape).is_empty()){
+    while (!(*_first_temp_tape).is_empty()) {
         (*_second_temp_tape).set_number((*_first_temp_tape).get_number());
         (*_first_temp_tape).go_forward();
         (*_second_temp_tape).go_forward();
@@ -127,38 +116,38 @@ void Sorter<T>::sort(std::string outFilePath) {
     (*_second_temp_tape).reset();
     //Раскидал блоки по двум начальным лентам
     int size_of_first_block, size_of_second_block;
-    for(int i = 0, c = block_size; i < (int)(log(N / c) / log(c)); i++){
+    for (int i = 0, c = block_size; i < (int) (log(N / c) / log(c)); i++) {
         first_tape_size = (N / block_size / 2 + N / block_size % 2) * block_size;
         second_tape_size = (N / block_size / 2) * block_size + N % block_size;
-        for(int j = 0; j < (N / (block_size * 2) / 2 + N / (block_size * 2) % 2) * (block_size * 2);){
-            if(first_tape_size == 0){
+        for (int j = 0; j < (N / (block_size * 2) / 2 + N / (block_size * 2) % 2) * (block_size * 2);) {
+            if (first_tape_size == 0) {
                 size_of_first_block = 0;
             } else {
                 size_of_first_block = block_size;
             }
-            if(second_tape_size == 0){
+            if (second_tape_size == 0) {
                 size_of_second_block = 0;
             } else {
                 size_of_second_block = block_size;
             }
-            while(size_of_first_block > 0 || size_of_second_block > 0){
-                if(size_of_second_block == 0){
+            while (size_of_first_block > 0 || size_of_second_block > 0) {
+                if (size_of_second_block == 0) {
                     new_number = (*_first_temp_tape).get_number();
                     (*_first_temp_tape).go_forward();
                     size_of_first_block--;
                     first_tape_size--;
-                }else if(size_of_first_block == 0){
+                } else if (size_of_first_block == 0) {
                     new_number = (*_second_temp_tape).get_number();
                     (*_second_temp_tape).go_forward();
                     size_of_second_block--;
                     second_tape_size--;
-                }else if((*_first_temp_tape).get_number() <= (*_second_temp_tape).get_number()){
+                } else if ((*_first_temp_tape).get_number() <= (*_second_temp_tape).get_number()) {
                     new_number = (*_first_temp_tape).get_number();
                     (*_first_temp_tape).go_forward();
                     size_of_first_block--;
                     first_tape_size--;
 
-                }else{
+                } else {
                     new_number = (*_second_temp_tape).get_number();
                     (*_second_temp_tape).go_forward();
                     size_of_second_block--;
@@ -167,41 +156,41 @@ void Sorter<T>::sort(std::string outFilePath) {
                 j++;
                 (*_first_trash_tape).set_number(new_number);
                 (*_first_trash_tape).go_forward();
-                if(first_tape_size == 0){
+                if (first_tape_size == 0) {
                     size_of_first_block = 0;
                 }
-                if(second_tape_size == 0){
+                if (second_tape_size == 0) {
                     size_of_second_block = 0;
                 }
             }
         }
         (*_first_trash_tape).reset();
-        for(int j = 0; j < (N / (block_size * 2) / 2) * 2 * block_size + N % (block_size * 2);){
+        for (int j = 0; j < (N / (block_size * 2) / 2) * 2 * block_size + N % (block_size * 2);) {
             size_of_first_block = block_size;
             size_of_second_block = block_size;
-            if(first_tape_size == 0){
+            if (first_tape_size == 0) {
                 size_of_first_block = 0;
             }
-            if(second_tape_size == 0){
+            if (second_tape_size == 0) {
                 size_of_second_block = 0;
             }
-            while(size_of_first_block > 0 || size_of_second_block > 0){
-                if(size_of_second_block == 0){
+            while (size_of_first_block > 0 || size_of_second_block > 0) {
+                if (size_of_second_block == 0) {
                     new_number = (*_first_temp_tape).get_number();
                     (*_first_temp_tape).go_forward();
                     first_tape_size--;
                     size_of_first_block--;
-                }else if(size_of_first_block == 0){
+                } else if (size_of_first_block == 0) {
                     new_number = (*_second_temp_tape).get_number();
                     (*_second_temp_tape).go_forward();
                     second_tape_size--;
                     size_of_second_block--;
-                }else if((*_first_temp_tape).get_number() <= (*_second_temp_tape).get_number()){
+                } else if ((*_first_temp_tape).get_number() <= (*_second_temp_tape).get_number()) {
                     new_number = (*_first_temp_tape).get_number();
                     (*_first_temp_tape).go_forward();
                     first_tape_size--;
                     size_of_first_block--;
-                }else{
+                } else {
                     new_number = (*_second_temp_tape).get_number();
                     (*_second_temp_tape).go_forward();
                     second_tape_size--;
@@ -210,10 +199,10 @@ void Sorter<T>::sort(std::string outFilePath) {
                 j++;
                 (*_second_trash_tape).set_number(new_number);
                 (*_second_trash_tape).go_forward();
-                if(first_tape_size == 0){
+                if (first_tape_size == 0) {
                     size_of_first_block = 0;
                 }
-                if(second_tape_size == 0){
+                if (second_tape_size == 0) {
                     size_of_second_block = 0;
                 }
             }
@@ -229,23 +218,23 @@ void Sorter<T>::sort(std::string outFilePath) {
     second_tape_size = (N / block_size / 2) * block_size + N % block_size;
     size_of_first_block = first_tape_size;
     size_of_second_block = first_tape_size;
-    while(size_of_first_block > 0 || size_of_second_block > 0){
-        if(size_of_second_block == 0){
+    while (size_of_first_block > 0 || size_of_second_block > 0) {
+        if (size_of_second_block == 0) {
             new_number = (*_first_temp_tape).get_number();
             (*_first_temp_tape).go_forward();
             first_tape_size--;
             size_of_first_block--;
-        }else if(size_of_first_block == 0){
+        } else if (size_of_first_block == 0) {
             new_number = (*_second_temp_tape).get_number();
             (*_second_temp_tape).go_forward();
             second_tape_size--;
             size_of_second_block--;
-        }else if((*_first_temp_tape).get_number() <= (*_second_temp_tape).get_number()){
+        } else if ((*_first_temp_tape).get_number() <= (*_second_temp_tape).get_number()) {
             new_number = (*_first_temp_tape).get_number();
             (*_first_temp_tape).go_forward();
             first_tape_size--;
             size_of_first_block--;
-        }else{
+        } else {
             new_number = (*_second_temp_tape).get_number();
             (*_second_temp_tape).go_forward();
             second_tape_size--;
@@ -253,10 +242,10 @@ void Sorter<T>::sort(std::string outFilePath) {
         }
         (*_output_tape).set_number(new_number);
         (*_output_tape).go_forward();
-        if(first_tape_size == 0){
+        if (first_tape_size == 0) {
             size_of_first_block = 0;
         }
-        if(second_tape_size == 0){
+        if (second_tape_size == 0) {
             size_of_second_block = 0;
         }
     }
